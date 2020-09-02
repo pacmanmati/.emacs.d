214b8ce41c9956295fcd0b7df9df827560b7e405
pacmanmati <pacmanmati@gmail.com>
2020-09-02 02:47:28 +0100

    create functions.el and add buffer+file rename function

---
+++ lisp/functions.el
@@ -0,0 +1,25 @@
+;;
+;; -- functions.el --
+;; useful standalone / helper functions.
+;;
+
+;; seemingly better rename function
+;; i found this somewhere on stackoverflow but forgot where
+;; not bound to a key since i dont use it often enough
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let* ((name (buffer-name))
+        (filename (buffer-file-name))
+        (basename (file-name-nondirectory filename)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " (file-name-directory filename) basename nil basename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          (message "File '%s' successfully renamed to '%s'"
+                   name (file-name-nondirectory new-name)))))))
